b61a9b0ad840b09b84e43f6c954e391cb17f63b7
whs <hswongac@gmail.com>
2020-04-30 07:49:59 +0800

    Add 2 more tests

--- index.ts
+++ index.ts
@@ -46,4 +46,63 @@ async function GitTest() {
   }
 }
 
+async function GitTest2() {
+  await rmdir("test2", { recursive: true })
+  await Git.Clone.clone("https://github.com/whs-dot-hk/merge-no-conflict-test.git", "test2", {
+    fetchOpts: {
+      callbacks: {
+        certificateCheck: () => 0
+      }
+    }
+  })
+
+  const repository = await Git.Repository.open("test2")
+
+  const ourBranchName = "master"
+  const theirBranchName = "refs/remotes/origin/add-dots"
+
+  const ourCommit = await repository.getBranchCommit(ourBranchName)
+  const theirCommit = await repository.getBranchCommit(theirBranchName)
+
+  const signature = Git.Signature.now("whs", "hswongac@gmail.com")
+
+  await repository.mergeBranches(ourBranchName, theirBranchName, signature)
+}
+
+async function GitTest3() {
+  await rmdir("test3", { recursive: true })
+  await Git.Clone.clone("https://github.com/whs-dot-hk/auto-merge-test.git", "test3", {
+    fetchOpts: {
+      callbacks: {
+        certificateCheck: () => 0
+      }
+    }
+  })
+
+  const repository = await Git.Repository.open("test3")
+
+  const ourBranchName = "master"
+  const remoteBranchName = "feature-1"
+
+  const targetCommit = await repository.getHeadCommit()
+  const reference = await repository.createBranch(remoteBranchName, targetCommit, false)
+
+  await repository.checkoutBranch(reference, {})
+  const commit = await repository.getReferenceCommit("refs/remotes/origin/" + remoteBranchName)
+
+  await Git.Reset.reset(repository, commit, 3, {})
+
+  await repository.checkoutBranch(ourBranchName, {})
+  const theirBranchName = "feature-1"
+
+  const ourCommit = await repository.getBranchCommit(ourBranchName)
+  const theirCommit = await repository.getBranchCommit(theirBranchName)
+
+  const signature = Git.Signature.now("whs", "hswongac@gmail.com")
+
+  await repository.mergeBranches(ourBranchName, theirBranchName, signature)
+}
+
 GitTest()
+GitTest2()
+GitTest3()
